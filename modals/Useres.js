a871217a3ee899a956d1b0db458b9215c82de899
aanitapandit123 <aanitapandit@gmail.com>
2019-12-16 14:01:31 +0545

    Routing

---
+++ modals/Useres.js
@@ -0,0 +1,22 @@
+//database schemas
+
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now
+
+    }
+});
