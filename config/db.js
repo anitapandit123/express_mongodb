40fa2f8a87b38a35ac41254584f272c71865e92e
aanitapandit123 <aanitapandit@gmail.com>
2019-12-27 13:12:45 +0545

    mongodb atlas connection sucessful

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+// const config = require('config');
+//const db = config.get('mongoURI');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(db, {
+        await mongoose.connect("mongodb+srv://anita123:anita@123@cluster0-uvqnb.mongodb.net/test?retryWrites=true&w=majority", {
             useNewUrlParser: true,
             useCreateIndex: true,
             useUnifiedTopology: true
