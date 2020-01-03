ff750caed558f63ffc715caeda6248f47b8953ad
aanitapandit123 <aanitapandit@gmail.com>
2020-01-03 17:10:15 +0545

    creating profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../../middleware/auth');
+const jwt = require('jsonwebtoken')
 
-const Profile = require('../../modals/Profiles');
+const auth = require('../../middleware/auth');
+const Profiles = require('../../modals/Profiles');
 const User = require('../../modals/User');
+const { check, validationResult } = require('express-validator');
 
 // @route GET api/profile/me
 // @desc GET current users profile
@@ -12,7 +14,7 @@ const User = require('../../modals/User');
 router.get('/me', auth, async (req, res) => {
 
     try {
-        const profile = await Profile.findOne({ user: req.user.id }).populate(
+        const profile = await Profiles.findOne({ user: req.user.id }).populate(
             'user',
             ['name', 'avatar']
         );
@@ -28,5 +30,23 @@ router.get('/me', auth, async (req, res) => {
         res.status(500).send('Server Error');
     }
 });
+
+
+//@route POST api/profile
+// @desc Create or update user profile
+
+router.post('/', [auth, [
+    check('status', 'Status is required')
+        .not()
+        .isEmpty(),
+    check('skills', 'skills is required')
+        .not()
+        .isEmpty(),
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+})
 
 module.exports = router;
