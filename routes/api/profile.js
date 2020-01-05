2366913cc1e5986c09eb67d0d63f60301676d9b6
aanitapandit123 <aanitapandit@gmail.com>
2020-01-05 18:38:42 +0545

    get user route by user id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -134,5 +134,22 @@ router.get('/', async (req, res) => {
     }
 });
 
+// Get single user by user id
+router.get('/user/:user_id', async (req, res) => {
+    try {
+        const profile = await (await Profiles.findOne({ user: req.params.user_id })).populate(
+            'user',
+            ['name', 'avatar']
+        ); if (!profile) return res.status(400).json({ msg: 'There is no profile for this user' }
+        );
+        res.json(profile)
+    }
+    catch (err) {
+        console.error(err.message);
+        res.status(500).send('SERVER ERROR')
+    }
+})
+
+
 
 module.exports = router;
