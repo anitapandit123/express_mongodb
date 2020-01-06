848da8804b1d44f4383a22db339450e4abfabc2f
aanitapandit123 <aanitapandit@gmail.com>
2020-01-06 15:20:44 +0545

    delete profile and  user by id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -139,16 +139,38 @@ router.get('/user/:user_id', async (req, res) => {
     try {
         const profile = await (await Profiles.findOne({ user: req.params.user_id })).populate(
             'user',
-            ['name', 'avatar']
-        ); if (!profile) return res.status(400).json({ msg: 'There is no profile for this user' }
-        );
+            ['name', 'avatar']);
+        if (!profile) return res.status(400).json({ msg: 'There is no profile for this user' });
         res.json(profile)
     }
     catch (err) {
         console.error(err.message);
+        if (err.kind == 'ObjectId') {
+            return res.status(400).json({ msg: 'Profile not found' });
+        }
         res.status(500).send('SERVER ERROR')
     }
-})
+});
+
+
+
+// @route DELETE api/profile
+// @desc Delete profile, user & posts
+
+router.delete('/', auth, async (req, res) => {
+    try {
+        // @todo -remove users posts
+        // Remove Profile
+        const profile = await Profiles.findOneAndRemove({ user: req.user.id });
+
+        //Remove User
+        await User.findOneAndRemove({ _id: req.user.id });
+        res.json({ msg: 'User Deleted' })
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("SERVER ERROR")
+    }
+});
 
 
 
