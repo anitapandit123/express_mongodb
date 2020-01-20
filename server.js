67c5097288f83fcca59512ee67a8d122111af506
aanitapandit123 <aanitapandit@gmail.com>
2020-01-20 19:14:19 +0545

    formdata blocked by Cors and fixed by adding access control headers

--- server.js
+++ server.js
@@ -7,9 +7,22 @@ const app = express();
 
 //connect database
 connectDB();
-
+//CORS block  formdata,thats why all these configuration are needed
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS");
+    // res.header('Access-Control-Allow-Origin', '*');
+    // res.header("Access-Control-Allow-Headers",
+    //     "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+    // if (req.method === 'OPTIONS') {
+    //     res.header('Access-Control-Allow-Origin', 'POST');
+    //     return res.status(200).json({});
+    // }
+
     next();
 });
 
