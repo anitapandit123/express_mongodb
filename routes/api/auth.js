42a3fdd8572678481aca582aed5e7b383d5b91f1
aanitapandit123 <aanitapandit@gmail.com>
2020-01-22 12:31:31 +0545

    bug fixed

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -9,6 +9,7 @@ router.get('/', auth, async (req, res) => {
     try {
         const user = await User.findById(req.user.id).select('-password')
         res.json(user);
+        console.log(user);
 
     } catch (err) {
         console.error(err.message);
