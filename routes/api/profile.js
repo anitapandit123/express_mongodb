7916629ecf5df7354fa41a11df8134f362f0ce42
aanitapandit123 <aanitapandit@gmail.com>
2020-03-05 15:51:14 +0545

    delete profile by experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -209,11 +209,23 @@ router.put('/experience',
             res.status(500).send('Server Error')
 
         }
+    });
+
+// @route DELETE api/profile/experience/exp_id
+// @desc Delete experience from profile
+// @access Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+    try {
+        // get the profile
+        const profile = await Profiles.findOne({ user: req.user.id });
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
     }
-);
+})
 
 
 
-//
 
 module.exports = router;
