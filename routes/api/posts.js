657c6f7303b2da015d164041bda037e6876a292a
aanitapandit123 <aanitapandit@gmail.com>
2020-03-10 14:36:20 +0545

    Like a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -99,5 +99,31 @@ router.delete('/:id', auth, async (req, res) => {
     }
 });
 
+//@route PUT api/posts/like/:id
+//@desc Like a post
+//@ccess Private
+
+router.put('/like/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        //Check if the post has already been liked
+        const userId = req.uer.id
+        if (
+            post.likes.filter(like => like.user.toString() === userId).length > 0
+        ) {
+            return res.status(400).json({ msg: 'Post already liked' });
+        }
+
+        post.likes.unshift({ user: req.user.id });
+
+        await post.save();
+
+        res.json(post.likes);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('SERVER ERROR');
+    }
+});
 
 module.exports = router;
